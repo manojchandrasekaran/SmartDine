90b18ea2e48805b5d17e84c77e5b437387ad55e8
Mano <mailformanoj.c@gmail.com>
2025-01-04 00:01:44 +0530

    Created project and added initial setup files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Smart Restaurant Food ordering System is under construction...</h1>
+      <h1>Will be serving you very soon!</h1>
+    </>
+  )
+}
+
+export default App
