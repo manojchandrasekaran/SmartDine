fcf83060bb2fc5571755dc39f3f8d641f9772f75
Mano <mailformanoj.c@gmail.com>
2025-02-01 23:32:15 +0530

    Redux toolkit Setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Counter from './features/Counter';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}></Route>
+
+        <Route path='/admin' element={<App />}>
+        
+        </Route>
         <Route path='/login' index element={<Login />}></Route>
         <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/counter' element={<Counter />}></Route>
       </Routes>
     </BrowserRouter>
   );
